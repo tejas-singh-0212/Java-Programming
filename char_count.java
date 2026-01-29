16b090d42890a1ef7520c946e5c06472fb1b5eda
Tejas Singh <tejassingh74823@gmail.com>
2026-01-29 21:30:20 +0530

    Counting characters in a string, ignoring whitespaces

---
+++ char_count.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class char_count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        sc.close();
+        int count = 0;
+        for (int i=0; i<str.length(); i++){
+            if(str.charAt(i) != ' ') count++;
+        }
+        System.out.println("Numbers of characters is "+count);
+    }
+}
